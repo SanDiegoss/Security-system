dd6eacdad6236b6e00973fc13a2d6d7985fa4278
Alexey <aleksei.nagaev@onlyoffice.com>
2022-12-19 21:03:09 +0300

    Backend ready

---
+++ backend/app.py
@@ -0,0 +1,110 @@
+from flask import Flask
+from flask import request
+import flask
+
+from sqlalchemy import Column
+from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+import pymysql
+
+import json
+import os
+from dataclasses import dataclass
+
+# create the extension
+db = SQLAlchemy()
+
+@dataclass
+class Log(db.Model):
+	id : int
+	date : int
+	message : str
+
+	id = Column(sqlalchemy.Integer, primary_key=True)
+	date = Column(sqlalchemy.Integer)
+	message = Column(sqlalchemy.String(256))
+
+@dataclass
+class Room(db.Model):
+	id : int
+	name : str
+	status : int
+
+	id = Column(sqlalchemy.Integer, primary_key=True)
+	name = Column(sqlalchemy.String(256))
+	status = Column(sqlalchemy.Integer)
+
+# take config (connectiong string)
+with open("config.json", "r") as jsonfile:
+	data = json.load(jsonfile)
+	connection_string = data["connection_string"]
+	static_folder = data["static_folder"]
+
+
+# create the app
+app = Flask(__name__, static_url_path='', static_folder=static_folder)
+
+# configure MySQL
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+
+# initialize the app with the extension
+db.init_app(app)
+
+with app.app_context():
+	db.create_all()
+
+# api
+@app.route("/api/GetRooms", methods = {"GET"})
+def GetRoooms():
+	rooms = db.session.query(Room).all()
+	return flask.jsonify(rooms), 200
+
+@app.route("/api/GetLogs", methods = {"GET"})
+def GetLogs():
+	logs = db.session.query(Log).all()
+	return flask.jsonify(logs), 200
+
+@app.route("/api/GetLogsCount", methods = {"GET"})
+def LogsCount():
+	count = db.session.query(Log).count()
+	return flask.jsonify(count), 200
+
+@app.route("/api/AddLog", methods = {"POST"})
+def AddLog():
+	if not request.is_json:
+		return 'BAD REQUEST', 400
+
+	if "message" not in request.json or "date" not in request.json:
+		return 'BAD REQUEST', 400
+
+	message = request.json["message"]
+	date = request.json["date"]
+
+	log = Log(
+		message = message,
+		date = date
+	)
+	db.session.add(log)
+	db.session.commit()
+	db.session.refresh(log)
+
+	return flask.jsonify(log.id), 200
+
+
+# react-app (SPA) default routing
+@app.route("/")
+def default():
+	return flask.send_file(static_folder + os.sep + 'index.html')
+
+#  starting...
+if __name__ == '__main__':
+	app.run(threading=True)
+
+
+
+	
+
+
+	
+
